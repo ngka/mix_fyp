e67aca5ab810b2b5dd955a828a95018c1c0947ae
DESKTOP-TKB7LV1\evang <210298728@stu.vtc.edu.hk>
2024-05-05 15:19:20 +0800

    ok

--- Mix_FYP/app/src/main/java/com/router/Constant.java
+++ Mix_FYP/app/src/main/java/com/router/Constant.java
@@ -7,3 +7,4 @@ public class Constant {
     public static final String BASE_URL = "https://308w35g695.yicp.fun/"; //(Home YW Wi-Fi)
     //public static final String BASE_URL = "http://172.20.10.9:8099/"; //iPhone
 }
+//
