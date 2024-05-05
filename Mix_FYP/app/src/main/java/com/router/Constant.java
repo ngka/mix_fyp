a71381cbe06aecf6ec0f42ccd5cc11f8d1971769
fypJCHT <168530938+fypJCHT@users.noreply.github.com>
2024-05-05 15:36:39 +0800

    Update Constant.java
    change the IP Address

--- Mix_FYP/app/src/main/java/com/router/Constant.java
+++ Mix_FYP/app/src/main/java/com/router/Constant.java
@@ -4,6 +4,7 @@ package com.router;
 
  常量*/
 public class Constant {
-    public static final String BASE_URL = "https://308w35g695.yicp.fun/"; //(Home YW Wi-Fi)
-    //public static final String BASE_URL = "http://172.20.10.9:8099/"; //iPhone
+    //public static final String BASE_URL = "https://308w35g695.yicp.fun/";
+    //public static final String BASE_URL = "http://192.168.0.239:8099/"; //(Home YW Wi-Fi)
+    public static final String BASE_URL = "http://172.20.10.9:8099/"; //iPhone
 }
